3b53c70ab4f343d8fc31c99ca0a2570940cd0ae9
The-Scripter-24 <78175442+The-Scripter-24@users.noreply.github.com>
2021-02-08 01:03:06 -0600

    Update 2000.js

--- src/2000.js
+++ src/2000.js
@@ -22,6 +22,26 @@ export const sac00 = {
 }
 
 export const invA00 = {
+    '17 + 9 - 13 + 23 =':'36',
+    '\\frac{3}{4} + \\frac{2}{3} = \\textrm{(improper fraction)}':'17/12',
+    '2001 \\times 17 + 3 =':'34020',
+    '3\\frac{3}{5}\\% = \\textrm{(fraction)}':'9/250',
+    '14 \\times 32 =':'448',
+    '931 - 139 =':'792',
+    '\\textrm{XLII} = \\textrm{(Arabic Numeral)}':'42',
+    '25 \\times 215 =':'5375',
+    '12 \\times 13 + 17 - 11 =':'162',
+    '549 \\times 62 =': 34038,
+    '\\textrm{The GCD of 78 and 114 is }': '6',
+    '34 * 34 =': '1156',
+    '23 \\times 15 + 33 \\times 15 =': '840',
+    '\\frac{5}{4} + \\frac{4}{5} = \\textrm{(mixed number)}': '2 1/20',
+    '23 + 28 + 33 + 38 + 43 + 48 =': '213',
+    '34 \\times 46 =': '1564',
+    '3\\frac{1}{3} \\times 63 =': '210',
+    '10 + 11 \\times 12 - 13 =': '129',
+    '\\textrm{The LCM of 42 and 48 is }': '336',
+    '\\textrm{What is square of the value of the following computation } 66 \\div 3 \\div 2 \\times 3': 1089
 
 }
 
