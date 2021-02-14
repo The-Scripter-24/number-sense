dcb6bef90d94723c9b55b4d75b582e3e74fbc75f
The-Scripter-24 <78175442+The-Scripter-24@users.noreply.github.com>
2021-02-13 21:54:50 -0600

    Update 2000.js

--- src/2000.js
+++ src/2000.js
@@ -8,10 +8,10 @@ export const sac00 = {
     '.075 = \\textrm{(fraction)}': '3/40',
     '242 \\div 9 = \\textrm{(mixed number)}':'26 8/9',
     '1 + 3 + 6 + 10 + 15 =': '35',
-    '13 + 159 + 347 + 891 - 10': 1400,
+    '13 + 159 + 347 + 891 - 10 =': 1400,
     '\\textrm{The sum of the positive integral divisors of 14 is}':'24',
     '12 \\times 17 =':'204',
-    '15 \\times \\frac{15}{17} = \\textrm{(mixed number)}': '13 4/17',
+    '15 \\times \\frac{15}{17} = \\textrm{(mixed number)}': '13 4/17',6
     '15 \\times 28 =': '420',
     '13 + 18 + 23 + 28 + 33 + 38 =': '153',
     '25 \\times 46 =': '1150',
@@ -26,6 +26,28 @@ export const invA00 = {
 }
 
 export const invB00 = {
+    '17 - 19 + 21 - 23 =': '-4',
+    '13 \\times 18 =': '234',
+    '1234 \\div 5 = \\textrm{(decimal)}': '246.8',
+    '25 \\times 157 =': '3925',
+    '60\\% + .8 - 75\\% = \\textrm{(fraction)}': '13/20',
+    '31 \\times 27 + 19 \\times 27 =': '1350',
+    'XC - XXV = \\textrm{(Arabic Numeral)}': '65',
+    '\\frac{7}{12} + \\frac{12}{7} = \\textrm{(mixed number)}' : '2 25/84',
+    '2001 \\times 15 - 15 =': '30000',
+    '119 + 701 - 351 + 931 =': 1400,
+    '\\frac{7}{8} = \\% \\textrm{(mixed number)}': '87 1/2',
+    '\\textrm{Which is smaller, } \\frac{3}{7} \\textrm{or} \\frac{5}{12} ': '5/12',
+    '713 - 317 =': '396',
+    '\\textrm{The LCM of 4, 6, and 8 is }': '24',
+    '4\\frac{1}{6} \\times 24\\% =':'1',
+    '1\\frac{1}{4}\\% = \\textrm{(fraction)}': '1/80',
+    '\\textrm{(The average of 21, 18, andd 33 is =)}': '24',
+    '3\\frac{1}{4} \\times 32 =': '104',
+    '\\textrm{The number of positive integral divisors of 20 is }': '6', 
+    '103635 \\div 147 \\div 3': 235
+    
+    
 
 }
 
